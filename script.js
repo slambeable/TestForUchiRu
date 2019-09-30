48227cfe677a44c9f1a9f1d6efe9a91687327c70
slambeable <41022916+slambeable@users.noreply.github.com>
2019-09-30 12:08:19 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    let result = 0;
+
+    function congradulations() {
+        $('html').hide();
+    };
+
+    function allButtonsReset(order) {
+        $('button').addClass('unpressed');
+        $('button').removeClass('pressed');
+        $('button').removeClass('uncorrectPressed');
+        $('#check').removeAttr('disabled');
+        $('#check').removeClass('checkError');
+        $('#check').addClass('lock');
+        if (order === 'NotAll') {
+            $('#tip').text('Это не все правильные ответы');
+            $('#tip').css('opacity', '1');
+          } else {
+            $('#tip').text('Вычисли x');
+            $('#tip').css('opacity', '1');
+          }
+          result = 0;
+    };
+
+    function checkState() {
+        if (result === 0) {
+            $('#check').addClass('lock');
+            $('#check').removeClass('unlock');
+            $('#check').attr('disabled');
+        } else {
+            $('#check').addClass('unlock');
+            $('#check').removeClass('lock');
+            $('#check').removeAttr('disabled');
+        }
+    };
+
+    $('button').click(function() {
+        $(this).toggleClass('unpressed');
+        $(this).toggleClass('pressed');
+        if ($(this).hasClass('pressed') && $(this).hasClass('correct')) {
+            result += 1;
+        }
+        if ($(this).hasClass('unpressed') && $(this).hasClass('correct')) {
+            result -= 1;
+        }
+        if ($(this).hasClass('pressed') && $(this).hasClass('uncorrect')) {
+            result -= 3;
+        }
+        if ($(this).hasClass('unpressed') && $(this).hasClass('uncorrect')) {
+            result += 3;
+        }
+        checkState();
+    })
+
+    $('#check').click(function() {
+        if (result === 2) {
+            $('#tip').css('opacity', '0');
+            $(this).addClass('checkPassed');
+            setTimeout(congradulations, 1500);
+        } else if (result === 1) {
+            $(this).addClass('checkError');
+            setTimeout(allButtonsReset, 1000, 'NotAll');
+        } else {
+            if ($('.uncorrect').hasClass('pressed')) {
+                $('.uncorrect.pressed').addClass('uncorrectPressed');
+                $('.uncorrect.pressed').removeClass('pressed');
+            }
+            $(this).addClass('checkError');
+            setTimeout(allButtonsReset, 1000, 'errAns');
+        }
+    });
+});
